12f8c7bbc82d2cbb4607ff152bb351f0165ca0de
Johnny Chang <johnny@johnnychang.com>
2015-12-05 17:23:42 -0600

    Added converted Markdown files for Github Wiki.

--- md.py
+++ md.py
@@ -1,5 +1,9 @@
 import markdown, gfm, os
 import mcq_hammertime
+import sys
+
+reload(sys)
+sys.setdefaultencoding('Cp1252')
 
 cur_dir = os.path.dirname(__file__)
 md_dir = os.path.join(cur_dir, "md-files")
@@ -7,13 +11,15 @@ output_dir = os.path.join(cur_dir, "output")
 
 # Get all markdown files in md-dir.
 files = [f for f in os.listdir(md_dir) if os.path.isfile(os.path.join(md_dir, f)) if f.endswith(".md")]
+# Initialize extension
+mcq = mcq_hammertime.MCExtension()
+md = markdown.Markdown(extensions = ['gfm', mcq])
 
-for f in files:
-    with open(f, "r") as f:
+for l in files:
+    with open(os.path.join(md_dir, l), "r") as f:
         string = f.read()
-        mcq = mcq_hammertime.MCExtension()
-        html = markdown.markdown(string, extensions=['gfm', mcq])
-        with open(os.path.join(output_dir, "synchronization-converted.html"), "w") as f_target:
+        html = md.reset().convert(string)
+        with open(os.path.join(output_dir, l.rstrip(".md") + ".html"), "w") as f_target:
             with open(os.path.join(cur_dir, "head.html"), "r") as head:
                 f_target.write(head.read())
             f_target.write(html)
