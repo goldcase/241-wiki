f587b2e5f38226ce9e87fa728fb80cb477b5a7bb
Johnny Chang <jychang3@fa15-cs241-015.cs.illinois.edu>
2015-12-06 15:09:04 -0600

    Updated md.py regarding Unicode.

--- md.py
+++ md.py
@@ -1,6 +1,7 @@
 import markdown, gfm, os
 import mcq_hammertime
 import sys
+from io import open
 
 reload(sys)
 sys.setdefaultencoding('Cp1252')
@@ -28,22 +29,22 @@ def sanitize(filename):
     return filename.replace(":", "").lower()
 
 for l in files:
-    with open(os.path.join(md_dir, l), "r") as f:
+    with open(os.path.join(md_dir, l), mode="r", encoding="utf8") as f:
         string = f.read()
         html = md.reset().convert(string)
         sanitized = sanitize(l)
         stripped = sanitized.rstrip(".md")
-        with open(os.path.join(output_dir, stripped + ".html"), "w") as f_target:
+        with open(os.path.join(output_dir, stripped + ".html"), mode="w") as f_target:
             f_target.write(head_text.format(l.rstrip(".md").replace("-", " ")) + html + tail_text)
 
 def create_index(filenames):
-    with open(os.path.join(output_dir, "index.html"), "w") as idx_file:
+    with open(os.path.join(output_dir, "index.html"), mode="w", encoding="utf8") as idx_file:
         idx_file.write(head_text.format("Table of Contents") + "<br /><ol>")
         for filename in filenames:
             html_filename = filename.rstrip(".md") + ".html"
             sanitized = sanitize(filename)
             stripped = sanitized.rstrip(".md") + ".html"
-            idx_file.write("<li><a href='{0}'>{1}</a></li><br />".format(stripped, html_filename))
+            idx_file.write(unicode("<li><a href='{0}'>{1}</a></li><br />".format(stripped, html_filename)))
         idx_file.write("</ol><br />" + tail_text)
 
 if __name__ == "__main__":
